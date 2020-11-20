759ed00000da8ff2a69d89366ab12d71ab3b1797
foolish-server <foolish.server@gmail.com>
2020-11-21 03:18:46 +0900

    Update version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,11 +3,13 @@ cd build/addons/sourcemod/scripting
 
 # Get plugin version
 export PLUGIN_VERSION=$(sed -En '/#define PLUGIN_VERSION\W/p' superzombiefortress.sp)
-echo 'PLUGIN_VERSION=$(echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*')' >> $GITHUB_ENV
+echo 'PLUGIN_VERSION=${{echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*'}}' >> $GITHUB_ENV
+
+
 
 # Get revision
 export PLUGIN_VERSION_REVISION=$(git rev-list --count HEAD)
-echo 'PLUGIN_VERSION_REVISION=$PLUGIN_VERSION_REVISION' >> $GITHUB_ENV
+echo 'PLUGIN_VERSION_REVISION=${{PLUGIN_VERSION_REVISION}}' >> $GITHUB_ENV
 
 # Set revision to superzombiefortress.sp
 sed -i -e 's/#define PLUGIN_VERSION_REVISION.*".*"/#define PLUGIN_VERSION_REVISION "'$PLUGIN_VERSION_REVISION'"/g' superzombiefortress.sp
